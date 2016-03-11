a58d903cbdb5bbcb9d2d6ad79ded14b7ab349101
Jian Sun <sun229@purdue.edu>
2016-03-10 21:28:40 -0500

    RAA calculation

---
+++ Dntupleana/MassFit/RaaCalculation.C
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <iomanip>
+#include <utility>
+#include <TFile.h>
+#include <TDirectoryFile.h>
+#include <TDirectory.h>
+#include <TTree.h>
+#include <TNtuple.h>
+#include <TH1D.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TH2D.h>
+#include <TString.h>
+#include <TProfile.h>
+#include <TEfficiency.h>
+#include <TGraph.h>
+#include <TGraphAsymmErrors.h>
+#include <TF1.h>
+#include <TCanvas.h>
+#include <TPad.h>
+#include <TLatex.h>
+#include <TLegend.h>
+#include <TLine.h>
+#include <TBox.h>
+#include <TCut.h>
+#include <TMath.h>
+#include <TRandom.h>
+#include <TRandom3.h>
+#include <TROOT.h>
+#include <TStyle.h>
+
+#define BRchain 0.0388
+
+using namespace std;
+
+//assuming sigma=70mb, Nevents=2213 millions, estimated MB efficiency=0.9  Lumi=2.213*10^9/(70*0.85*10^9pb)=2.213/(70*0.9)=0.0351
+const float LumiMBpp = 0.0351;
+//assuming sigma=7750 mb, Nevents=1.0 millions, estimated MB efficiency=0.9  Lumi=0.001*10^9/(7750*0.90*10^9pb)=0.001/(7750*0.9)=7.20*1e-6
+const float LumiMBPbPbpermillionevt = 1.43369e-07;
+
+void RaaCalculation(TString fileinputppdata = "rootfiles/EfficiencyandCorrectedSpectrum_PbPb0_MBtrig_cent0to100.root", TString fileinputPbPbdata = "rootfiles/EfficiencyandCorrectedSpectrum_PbPb1_MBtrig_cent0to100.root", TString MBorDtrig = "MBtrig", int centlow = 0, int centhigh = 100, float ptmin = 2.0, float ptmax = 35.0)
+{
+	gStyle->SetOptTitle(0);
+	gStyle->SetOptStat(0);
+
+	TFile * inputppdata = new TFile(fileinputppdata); 
+	TFile * inputPbPbdata = new TFile(fileinputPbPbdata);
+
+	TH1D * dNdpt_data_corrected_pp = (TH1D *) inputppdata->Get("dNdpt_data_corrected");
+	TH1D * dNdpt_data_corrected_PbPb = (TH1D *) inputPbPbdata->Get("dNdpt_data_corrected");
+	TH1D * h_hibin = (TH1D *) inputPbPbdata->Get("h_hiBin");
+	const float LumiMBPbPb = LumiMBPbPbpermillionevt * h_hibin->GetEntries()/1.0e6;
+
+	TH1D * dsigmadpt_data_pp = (TH1D *) dNdpt_data_corrected_pp->Clone("dsigmadpt_data_pp");
+	dsigmadpt_data_pp->Scale( 0.5 * 1.0/BRchain * 1.0/LumiMBpp);
+
+	TH1D * dsigmadpt_data_PbPb_overNcoll = (TH1D *) dNdpt_data_corrected_PbPb->Clone("dNdpt_data_corrected_PbPb");
+	dsigmadpt_data_PbPb_overNcoll->Scale(0.5 * 1.0/BRchain * 1.0/LumiMBPbPb * 1.0/(208*208));
+
+	TH1D * RAAD0 = (TH1D *) dsigmadpt_data_pp->Clone("RAAD0");
+	RAAD0->Divide( dsigmadpt_data_PbPb_overNcoll, dsigmadpt_data_pp, 1.0, 1.0);
+
+	TCanvas * cfg_RAA = new TCanvas("cfg_RAA","cfg_RAA");
+
+	RAAD0->GetXaxis()->SetRangeUser(ptmin+0.2, ptmax-0.2);
+	RAAD0->GetYaxis()->SetRangeUser(0,1.5);
+	RAAD0->GetXaxis()->SetTitle("p_{T} (GeV/c)");
+	RAAD0->GetYaxis()->SetTitle("D^{0} R_{AA}");
+
+	RAAD0->SetMarkerStyle(20);
+	RAAD0->SetMarkerSize(1.0);
+	RAAD0->SetMarkerColor(4.0);
+	RAAD0->SetLineColor(4.0);
+	RAAD0->Draw();
+
+	TF1 * fun = new TF1("fun", "1.0", 0, 500);
+	fun->SetLineColor(1.0);
+	fun->SetLineStyle(3);
+	fun->SetLineWidth(0);
+	fun->Draw("same");
+
+	cfg_RAA->SaveAs(Form("Plots_RAA/cfg_cfg_RAA_%s_cent%dto%d.pdf", MBorDtrig.Data(), centlow, centhigh));
+
+	TFile * output = new TFile(Form("rootfiles/RAA_and_crosssection_%s_cent%dto%d.root", MBorDtrig.Data(), centlow, centhigh),"RECREATE");
+
+	dsigmadpt_data_pp->Write();
+	dsigmadpt_data_PbPb_overNcoll->Write();
+	RAAD0->Write();
+
+	output->Close();
+}
--- Dntupleana/MassFit/Run_Raa.sh
+++ Dntupleana/MassFit/Run_Raa.sh
@@ -2,6 +2,7 @@
 
 DoMassFit=1
 DoEfficiencyandCorrectedSpectrum=1
+DoRaaCrosssection=1
 
 InputppMBdata="./../rootfiles/anaDntuple_skim_ncand_D0Dntuple_crab_pp_ALLMinimumBias_AOD_D0_tkpt0p5_Ds_01212016.root"
 InputPbPbMBdata="./../rootfiles/anaDntuple_Dntuple_crab_PbPb_HIMinimumBias1to7_ForestAOD_highpuritytk_D0_tkpt0p7eta1p5_goldenjson_02222016_Cent-0to100_TKPT1p0.root"
@@ -10,6 +11,9 @@ InputPbPbMC="./../rootfiles/anaDntuple_ntD_EvtBase_20160303_Dfinder_20160302_PbP
 
 ppdataMBRaw="rootfiles/Raw_spectrum_anaDntuple_skim_ncand_D0Dntuple_crab_pp_ALLMinimumBias_AOD_D0_tkpt0p5_Ds_01212016.root"
 PbPbdataMBRaw="rootfiles/Raw_spectrum_anaDntuple_Dntuple_crab_PbPb_HIMinimumBias1to7_ForestAOD_highpuritytk_D0_tkpt0p7eta1p5_goldenjson_02222016_Cent-0to100_TKPT1p0.root"
+
+ppdatacorrected="rootfiles/EfficiencyandCorrectedSpectrum_PbPb0_MBtrig_cent0to100.root"
+PbPbdatacorrected="rootfiles/EfficiencyandCorrectedSpectrum_PbPb1_MBtrig_cent0to100.root"
 
 MBorDtrig="MBtrig"
 iptstart=2
@@ -24,6 +28,10 @@ fi
 if [ $DoEfficiencyandCorrectedSpectrum -eq 1 ]; then
 	root -l -b -q 'EfficiencyandCorrectedSpectrum.C++("'$InputppMC'","'$ppdataMBRaw'",0,"'$MBorDtrig'",0,100,2.0,35.0)'
 	root -l -b -q 'EfficiencyandCorrectedSpectrum.C++("'$InputPbPbMC'","'$PbPbdataMBRaw'",1,"'$MBorDtrig'",0,100,2.0,35.0)'
+fi
+
+if [ $DoRaaCrosssection -eq 1 ]; then
+	root -l -b -q 'RaaCalculation.C++("'$ppdatacorrected'", "'$PbPbdatacorrected'", "'$MBorDtrig'", 0, 100, 2.0, 35.0)'
 fi
 
 ./cleanup.sh
