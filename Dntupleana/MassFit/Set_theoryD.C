075a7ba7877c042dadebd2d7b47f418f91cb6af6
Jian Sun <sun229@purdue.edu>
2017-03-24 13:34:40 -0400

    update theory calculations

--- Dntupleana/MassFit/Set_theoryD.C
+++ Dntupleana/MassFit/Set_theoryD.C
@@ -18,13 +18,73 @@
 #include <TGraphAsymmErrors.h>
 #include <TMultiGraph.h>
 
-TGraphAsymmErrors * gv2Dmeson5TeV_CUJET3[3];
 TGraphAsymmErrors * gv2Dmeson5TeV_TAMU[3];
+
+//old calculations
+//TGraphAsymmErrors * gv2Dmeson5TeV_CUJET3[3];
+//new CUJET
+TGraphErrors * gv2Dmeson5TeV_CUJET3[3];
 
 TGraph * gv2Dmeson5TeV_LBT[3];
 TGraph * gv3Dmeson5TeV_LBT[3];
 
 TGraph * gv2Dmeson5TeV_LPang[3];
+
+TGraph * gv2Dmeson5TeV_SUBATECH[3];
+TGraph * gv3Dmeson5TeV_SUBATECH[3];
+
+TGraph * gv2Dmeson5TeV_PHSD[3];
+TGraph * gv3Dmeson5TeV_PHSD[3];
+
+void Set_PHSD()
+{
+	TFile * inputTAMU = new TFile("TheoryCalculation/PHSD/PHSD_v2v3_interpolation_from0p5.root");
+	gv2Dmeson5TeV_PHSD[0] = (TGraph *) inputTAMU->Get("gv2Dmeson5TeV_PHSD_cent0to10");
+	gv2Dmeson5TeV_PHSD[1] = (TGraph *) inputTAMU->Get("gv2Dmeson5TeV_PHSD_cent10to30");
+	gv2Dmeson5TeV_PHSD[2] = (TGraph *) inputTAMU->Get("gv2Dmeson5TeV_PHSD_cent30to50");
+	gv3Dmeson5TeV_PHSD[0] = (TGraph *) inputTAMU->Get("gv3Dmeson5TeV_PHSD_cent0to10");
+	gv3Dmeson5TeV_PHSD[1] = (TGraph *) inputTAMU->Get("gv3Dmeson5TeV_PHSD_cent10to30");
+	gv3Dmeson5TeV_PHSD[2] = (TGraph *) inputTAMU->Get("gv3Dmeson5TeV_PHSD_cent30to50");
+
+	for( int ibin = 0; ibin < 3; ibin++ )
+	{
+		gv2Dmeson5TeV_PHSD[ibin]->SetLineWidth(2);
+		gv2Dmeson5TeV_PHSD[ibin]->SetLineColor(kMagenta+1);
+		gv2Dmeson5TeV_PHSD[ibin]->SetLineStyle(2);
+		
+		gv3Dmeson5TeV_PHSD[ibin]->SetLineWidth(2);
+		gv3Dmeson5TeV_PHSD[ibin]->SetLineColor(kMagenta+1);
+		gv3Dmeson5TeV_PHSD[ibin]->SetLineStyle(2);
+	}
+}
+
+void Set_SUBATECH()
+{
+	gv2Dmeson5TeV_SUBATECH[0] = new TGraph("TheoryCalculation/SUBATECH_updated/5TeV0-10v2D_CMS.dat");
+	gv2Dmeson5TeV_SUBATECH[1] = new TGraph("TheoryCalculation/SUBATECH_updated/5TeV10-30v2D_CMS.dat");
+	gv2Dmeson5TeV_SUBATECH[2] = new TGraph("TheoryCalculation/SUBATECH_updated/5TeV30-50v2D_CMS.dat");
+	gv2Dmeson5TeV_SUBATECH[0]->SetName("gv2Dmeson5TeV_SUBATECH_cent0to10");
+	gv2Dmeson5TeV_SUBATECH[1]->SetName("gv2Dmeson5TeV_SUBATECH_cent10to30");
+	gv2Dmeson5TeV_SUBATECH[2]->SetName("gv2Dmeson5TeV_SUBATECH_cent30to50");
+
+	gv3Dmeson5TeV_SUBATECH[0] = new TGraph("TheoryCalculation/SUBATECH_updated/5TeV0-10v3D_CMS.dat");
+	gv3Dmeson5TeV_SUBATECH[1] = new TGraph("TheoryCalculation/SUBATECH_updated/5TeV10-30v3D_CMS.dat");
+	gv3Dmeson5TeV_SUBATECH[2] = new TGraph("TheoryCalculation/SUBATECH_updated/5TeV30-50v3D_CMS.dat");
+	gv3Dmeson5TeV_SUBATECH[0]->SetName("gv3Dmeson5TeV_SUBATECH_cent0to10");
+	gv3Dmeson5TeV_SUBATECH[1]->SetName("gv3Dmeson5TeV_SUBATECH_cent10to30");
+	gv3Dmeson5TeV_SUBATECH[2]->SetName("gv3Dmeson5TeV_SUBATECH_cent30to50");
+
+	for( int ibin = 0; ibin < 3; ibin++ )
+	{
+		gv2Dmeson5TeV_SUBATECH[ibin]->SetLineWidth(2);
+		gv2Dmeson5TeV_SUBATECH[ibin]->SetLineColor(67);
+		gv2Dmeson5TeV_SUBATECH[ibin]->SetLineStyle(5);
+		
+		gv3Dmeson5TeV_SUBATECH[ibin]->SetLineWidth(2);
+		gv3Dmeson5TeV_SUBATECH[ibin]->SetLineColor(67);
+		gv3Dmeson5TeV_SUBATECH[ibin]->SetLineStyle(5);
+	}
+}
 
 void Set_LPang()
 {
@@ -37,7 +97,7 @@ void Set_LPang()
 
 	for( int ibin = 0; ibin < 3; ibin++ )
 	{
-		gv2Dmeson5TeV_LPang[ibin]->SetLineWidth(2);
+		gv2Dmeson5TeV_LPang[ibin]->SetLineWidth(1);
 		//gv2Dmeson5TeV_LPang[ibin]->SetLineColor(kOrange-2);
 		gv2Dmeson5TeV_LPang[ibin]->SetLineColor(kMagenta+2);
 	}
@@ -61,11 +121,15 @@ void Set_LBT()
 
 	for( int ibin = 0; ibin < 3; ibin++ )
 	{
+		//gv2Dmeson5TeV_LBT[ibin]->SetLineWidth(1);
 		gv2Dmeson5TeV_LBT[ibin]->SetLineWidth(2);
 		gv2Dmeson5TeV_LBT[ibin]->SetLineColor(4);
+		gv2Dmeson5TeV_LBT[ibin]->SetLineStyle(9);
 		
+		//gv3Dmeson5TeV_LBT[ibin]->SetLineWidth(1);
 		gv3Dmeson5TeV_LBT[ibin]->SetLineWidth(2);
 		gv3Dmeson5TeV_LBT[ibin]->SetLineColor(4);
+		gv3Dmeson5TeV_LBT[ibin]->SetLineStyle(9);
 	}
 }
 
@@ -80,25 +144,47 @@ void Set_TAMU()
 	{
 		gv2Dmeson5TeV_TAMU[ibin]->SetFillStyle(1001);
 		//gv2Dmeson5TeV_TAMU[ibin]->SetFillStyle(3001);
-		gv2Dmeson5TeV_TAMU[ibin]->SetFillColor(14);
-		gv2Dmeson5TeV_TAMU[ibin]->SetLineColor(14);
+		gv2Dmeson5TeV_TAMU[ibin]->SetFillColor(28);
+		gv2Dmeson5TeV_TAMU[ibin]->SetLineColor(28);
 		gv2Dmeson5TeV_TAMU[ibin]->SetLineWidth(2);
 	}
 }
 
+//CUJET old
+//void Set_CUJET3()
+//{
+//	TFile * inputCUJET3 = new TFile("TheoryCalculation/CUJet3p0/PredictionsCUJET3_Dv2_pt.root");
+//	gv2Dmeson5TeV_CUJET3[0] = ( TGraphAsymmErrors * ) inputCUJET3->Get("gv2Dmeson5TeV_CUJET3_cent0to10");
+//	gv2Dmeson5TeV_CUJET3[1] = ( TGraphAsymmErrors * ) inputCUJET3->Get("gv2Dmeson5TeV_CUJET3_cent10to30");
+//	gv2Dmeson5TeV_CUJET3[2] = ( TGraphAsymmErrors * ) inputCUJET3->Get("gv2Dmeson5TeV_CUJET3_cent30to50");
+//
+//	for( int ibin = 0; ibin < 3; ibin++ )
+//	{
+//		gv2Dmeson5TeV_CUJET3[ibin]->SetLineColor(8);
+//		gv2Dmeson5TeV_CUJET3[ibin]->SetLineWidth(1);
+//		gv2Dmeson5TeV_CUJET3[ibin]->SetFillStyle(1001);
+//		gv2Dmeson5TeV_CUJET3[ibin]->SetFillColor(kGreen-10);
+//	}
+//}
+
+//updated CUJET
 void Set_CUJET3()
 {
-	TFile * inputCUJET3 = new TFile("TheoryCalculation/CUJet3p0/PredictionsCUJET3_Dv2_pt.root");
-	gv2Dmeson5TeV_CUJET3[0] = ( TGraphAsymmErrors * ) inputCUJET3->Get("gv2Dmeson5TeV_CUJET3_cent0to10");
-	gv2Dmeson5TeV_CUJET3[1] = ( TGraphAsymmErrors * ) inputCUJET3->Get("gv2Dmeson5TeV_CUJET3_cent10to30");
-	gv2Dmeson5TeV_CUJET3[2] = ( TGraphAsymmErrors * ) inputCUJET3->Get("gv2Dmeson5TeV_CUJET3_cent30to50");
-
+	TFile * inputCUJET3 = new TFile("TheoryCalculation/CUJET3_updated/CUJET_v2.root");
+	
+	gv2Dmeson5TeV_CUJET3[0] = ( TGraphErrors * ) inputCUJET3->Get("gv2Dmeson5TeV_CUJET_cent0to10");
+	gv2Dmeson5TeV_CUJET3[1] = ( TGraphErrors * ) inputCUJET3->Get("gv2Dmeson5TeV_CUJET_cent10to30");
+	gv2Dmeson5TeV_CUJET3[2] = ( TGraphErrors * ) inputCUJET3->Get("gv2Dmeson5TeV_CUJET_cent30to50");
+	
 	for( int ibin = 0; ibin < 3; ibin++ )
 	{
-		gv2Dmeson5TeV_CUJET3[ibin]->SetLineColor(8);
-		gv2Dmeson5TeV_CUJET3[ibin]->SetLineWidth(2);
-		gv2Dmeson5TeV_CUJET3[ibin]->SetFillStyle(1001);
-		gv2Dmeson5TeV_CUJET3[ibin]->SetFillColor(kGreen-10);
+		gv2Dmeson5TeV_CUJET3[ibin]->SetLineColor(kGreen-10);
+		gv2Dmeson5TeV_CUJET3[ibin]->SetLineWidth(0);
+		gv2Dmeson5TeV_CUJET3[ibin]->SetFillStyle(3002);
+		gv2Dmeson5TeV_CUJET3[ibin]->SetFillColor(kGreen-7);
+		gv2Dmeson5TeV_CUJET3[ibin]->SetLineColor(kGreen-7);
+		//gv2Dmeson5TeV_CUJET3[ibin]->SetFillColor(kGreen-10);
+		//gv2Dmeson5TeV_CUJET3[ibin]->SetFillStyle(1001);
 	}
 }
 
@@ -108,4 +194,6 @@ void Set_theoryD()
 	Set_TAMU();
 	Set_LBT();
 	Set_LPang();
+	Set_SUBATECH();
+	Set_PHSD();
 }
